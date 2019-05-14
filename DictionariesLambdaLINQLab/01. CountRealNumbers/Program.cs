739aee825488bbe4b8370380c693beab925e12c9
datavelizar <velizar_s@mail.bg>
2019-05-15 01:28:59 +0300

    added Dictionaries and LINQ folder

---
+++ DictionariesLambdaLINQLab/01. CountRealNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿//Read a list of integers and print them in ascending order along with their number of occurrences.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (int number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
