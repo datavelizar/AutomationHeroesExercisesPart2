ccc5871fe4d678c85071e5c373cf56e66d248821
datavelizar <velizar_s@mail.bg>
2019-05-16 06:33:04 +0300

    added task 03, 04, 05

---
+++ DictionariesLambdaLINQLab/03. WordSynonyms/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._WordSynonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<string>> synonymDictionary = new Dictionary<string, List<string>>();
+            int numberOfPairLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPairLines; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonymDictionary.ContainsKey(word))
+                {
+                    synonymDictionary.Add(word, new List<string> { synonym });
+                }
+                else
+                {
+                    synonymDictionary[word].Add(synonym);
+                }
+            }
+
+            foreach (var word in synonymDictionary)
+            {
+                Console.Write($"{word.Key} - ");
+
+                for (int i = 0; i < word.Value.Count; i++)
+                {
+                    if (i < word.Value.Count - 1)
+                    {
+                        Console.Write($"{ word.Value[i]}, ");
+                    }
+                    else
+                    {
+                        Console.Write($"{ word.Value[i]}");
+                    }
+
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
---
+++ DictionariesLambdaLINQLab/05. WordFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿//Read an array of strings, take only words which length is even. Print each word on a new line.
+
+using System;
+using System.Linq;
+
+namespace _05._WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").Where(w => w.Length % 2 == 0).ToArray();
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
