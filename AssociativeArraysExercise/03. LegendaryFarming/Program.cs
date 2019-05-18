30d47263ae9a7c8a2f610c944bb00cfb1b1a1044
datavelizar <velizar_s@mail.bg>
2019-05-18 05:08:06 +0300

    added task 3 Legendary Farming

--- AssociativeArraysExercise/03. LegendaryFarming/Program.cs
+++ AssociativeArraysExercise/03. LegendaryFarming/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._LegendaryFarming
 {
@@ -7,44 +8,103 @@ namespace _03._LegendaryFarming
     {
         static void Main(string[] args)
         {
+            int quantity = 0;
+            string material = null;
+            string[] keyMaterialsTexts = { "shards", "fragments", "motes" };
+            bool isFinished = false;
+
+            Dictionary<string, int> keyMaterials = new Dictionary<string, int>();
+
+            keyMaterials.Add("shards", 0);
+            keyMaterials.Add("fragments", 0);
+            keyMaterials.Add("motes", 0);
+
+            Dictionary<string, int> junk = new Dictionary<string, int>();
+
             while (true)
             {
+                string input = Console.ReadLine();//"3 Motes 5 stones 5 Shards"; //
 
-            
-            string input = "3 Motes 5 stones 5 Shards"; //Console.ReadLine();
+                string[] inputArr = input.Split(" ");
 
-            string[] inputArr = input.Split(" ");
+                for (int i = 0; i < inputArr.Length; i++)
+                {
+                    if (i % 2 == 0) { quantity = int.Parse(inputArr[i]); }
+                    if (i % 2 == 1)
+                    {
+                        material = inputArr[i].ToLower();
 
-            int quantity = 0;
-            string material = "";
+                        if (keyMaterialsTexts.Any(x => x == material))
+                        {
+                            keyMaterials[material] += quantity;
+                            if (keyMaterials["shards"] >= 250)
+                            {
+                                isFinished = true;
+                                keyMaterials["shards"] -= 250;
+                                Console.WriteLine("Shadowmourne obtained!");
+                                PrintKeyMaterialsDescending(keyMaterials);
+                                break;
+                            }
 
-            Dictionary<string, int> materials = new Dictionary<string, int>();
+                            if (keyMaterials["fragments"] >= 250)
+                            {
+                                isFinished = true;
+                                keyMaterials["fragments"] -= 250;
+                                Console.WriteLine("Valanyr obtained!");
+                                PrintKeyMaterialsDescending(keyMaterials);
+                                break;
+                            }
 
-            for (int i = 0; i < inputArr.Length; i++)
+                            if (keyMaterials["motes"] >= 250)
+                            {
+                                isFinished = true;
+                                keyMaterials["motes"] -= 250;
+                                Console.WriteLine("Dragonwrath obtained!");
+                                PrintKeyMaterialsDescending(keyMaterials);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (junk.ContainsKey(material))
+                            {
+                                junk[material] += quantity;
+                            }
+                            else
+                            {
+                                junk.Add(material, quantity);
+                            }
+                        }
+                    }
+                }
+
+                if (isFinished) { break; }
+            }
+
+            PrintJunk(junk);
+        }
+
+        private static void PrintJunk(Dictionary<string, int> junk)
+        {
+            foreach (var item in junk.OrderBy(r => r.Key))
             {
-                if (i % 2 == 0) { quantity = int.Parse(inputArr[i]); }
-                else { material = inputArr[i].ToLower(); }
+                Console.WriteLine($"{ item.Key}: {item.Value}");
+            }
+        }
 
-                if (materials.ContainsKey(material))
-                {
-                    materials[material] += quantity;
-
-                    
-                }
-                else
-                {
-                    materials.Add(material, 0);
-                }
-
-                }
+        private static void PrintKeyMaterialsDescending(Dictionary<string, int> keyMaterials)
+        {
+            foreach (var item in keyMaterials.OrderByDescending(key => key.Value).ThenBy(r => r.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
             }
         }
     }
+}
 
-    public enum KeyMaterials
-    {
-        shards = 0,
-        fragments = 1,
-        motes = 2
-    }
+public enum KeyMaterials
+{
+    shards = 0,
+    fragments = 1,
+    motes = 2
 }
