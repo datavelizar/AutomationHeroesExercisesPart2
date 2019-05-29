1d827269cf8729990d0559f9af512d107a44a11e
datavelizar <velizar_s@mail.bg>
2019-05-29 03:13:32 +0300

    WIP P08 Military Elite

---
+++ CSharp-OOP-Interfaces-And-Abstraction-Exercises/P08. Military Elite/Models/Private.cs
@@ -0,0 +1,25 @@
+﻿namespace P08._Military_Elite.Models
+{
+    using P08._Military_Elite.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class Private : IPrivate
+    {
+        public double Salary { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public override string ToString()
+        {
+            //Name: < firstName > < lastName > Id: < id > Salary: < salary >
+            var sb = new StringBuilder();
+
+            sb.Append(string.Format($"Name: {this.FirstName} {this.LastName} Id: {this.Id} Salary: {this.Salary}"));
+            return sb.ToString();
+        }
+    }
+}
